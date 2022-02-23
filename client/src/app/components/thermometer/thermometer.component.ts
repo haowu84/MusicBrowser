0ecfffc9f526f0e9625afab5ee718bfd082bc43b
Hao Wu <32400236+haowu84@users.noreply.github.com>
2022-02-23 12:34:38 -0800

    Add files via upload

---
+++ client/src/app/components/thermometer/thermometer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-thermometer",
+  templateUrl: "./thermometer.component.html",
+  styleUrls: ["./thermometer.component.css"],
+})
+export class ThermometerComponent implements OnInit {
+  //TODO: define Input fields and bind them to the template.
+  @Input() color: string;
+  @Input() percent: string;
+  @Input() name: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ client/src/app/data/resource-data.ts
@@ -0,0 +1,25 @@
+export abstract class ResourceData {
+	category:string = "unknown";
+	name:string;
+	imageURL:string;
+	id:string;
+	url:string;
+	embed: string;
+
+	constructor(objectModel:{}) {
+		this.name = objectModel['name'];
+		this.id = objectModel['id'];
+		if(objectModel['images'] && objectModel['images'].length > 0) {
+			this.imageURL = objectModel['images'][0].url;
+		} else if(objectModel['album'] && objectModel['album']['images'] && objectModel['album']['images'].length > 0) {
+			this.imageURL = objectModel['album']['images'][0].url;
+		} else {
+			this.imageURL = '../../assets/unknown.jpg';
+		}
+
+		if('spotify' in objectModel['external_urls']) {
+			this.url = objectModel['external_urls']['spotify'];
+			this.embed = this.url.substring(0, 24) + "/embed" + this.url.substring(24);
+		}
+	}
+}
